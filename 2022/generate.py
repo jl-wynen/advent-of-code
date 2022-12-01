381400b83677e097fe3dc3022c506b870db20e37
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-12-01 18:20:59 +0100

    Update stack.yaml automatically

--- 2022/generate.py
+++ 2022/generate.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import sys
+import yaml
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -15,12 +16,22 @@ def process_file(infile, target_dir, day):
         f.write(template.format(day=d))
 
 
+def update_stack_file(day):
+    with open(BASE_DIR/"stack.yaml", 'r') as f:
+        stack = yaml.safe_load(f)
+    if (s := f's{day:02d}') not in stack['packages']:
+        stack['packages'].append(s)
+    with open(BASE_DIR/"stack.yaml", 'w') as f:
+        yaml.dump(stack, f)
+    
+
 def main():
     day = int(sys.argv[1])
     target = BASE_DIR / f's{day:02d}'
     target.mkdir()
     for infile in (BASE_DIR / 'template').iterdir():
         process_file(infile, target, day)
+    update_stack_file(day)
 
 
 if __name__ == '__main__':
