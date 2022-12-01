14f882196f556e9d9c29bb0173d35dc158acc631
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-12-01 13:08:45 +0100

    2022-01

--- 2022/s01/Main.hs
+++ 2022/s01/Main.hs
@@ -1,2 +1,34 @@
+import Paths_s01
+import Data.List (sortOn)
+
+readInput :: IO (String)
+readInput = do
+    inputFile <- getDataFileName "input"
+    readFile inputFile
+
+
+dropFirst :: [String] -> [String]
+dropFirst [] = []
+dropFirst (_:t) = t
+
+
+groupElves :: [String] -> [[String]]
+groupElves [] = []
+groupElves l =
+    let (a, b) = break (=="") l
+    in a : (groupElves $ dropFirst b)
+
+toInt :: String -> Int
+toInt s = read s
+
+elveCalories :: String -> [[Int]]
+elveCalories x = map (map toInt) (groupElves $ lines x)
+
 main :: IO ()
-main = putStrLn "Day 01"
+main = do
+    input <- readInput
+    let calories = map sum $ elveCalories input
+    putStr "Part 1: maximum calories  = "
+    print $ maximum $ calories
+    putStr "Part 2: calories of top 3 = "
+    print $ sum $ take 3 $ sortOn negate calories
