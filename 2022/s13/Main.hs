554cdef3d2e88d949e6d63049d4268b8a8a055b3
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-12-15 19:49:55 +0100

    Attempt to debug 2022-13

--- 2022/s13/Main.hs
+++ 2022/s13/Main.hs
@@ -1,18 +1,27 @@
 import           FileInput (parseInput, Packet (..))
 
-packetOrder :: (Packet, Packet) -> Ordering
-packetOrder (Item a, Item b) = a `compare` b
-packetOrder (List [], _) = LT
+import Debug.Trace (trace)
+
+data Result = InOrder | OutOfOrder | KeepGoing deriving (Eq)
+
+packetOrder :: (Packet, Packet) -> Result
+packetOrder (a,b) | trace ("packetOrder " ++ show a ++ "\n            " ++ show b) False = undefined
+packetOrder (Item a, Item b)
+    | a < b = InOrder
+    | a > b = OutOfOrder
+    | otherwise = KeepGoing
 packetOrder (Item a, List b) = packetOrder (List [Item a], List b)
 packetOrder (List a, Item b) = packetOrder (List a, List [Item b])
+packetOrder (List [], List []) = KeepGoing
+packetOrder (List [], _) = InOrder
+packetOrder (_, List []) = OutOfOrder
 packetOrder (List (a:as), List (b:bs)) = case packetOrder (a, b) of
-    LT -> LT
-    GT -> GT
-    EQ -> packetOrder (List as, List bs)
-packetOrder _ = GT
+    KeepGoing -> packetOrder (List as, List bs)
+    x -> x
 
 correctOrder :: (Packet, Packet) -> Bool
-correctOrder x = packetOrder x /= GT
+correctOrder x = packetOrder x /= OutOfOrder
+
 
 main :: IO ()
 main = do
