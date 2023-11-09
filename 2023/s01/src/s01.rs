9221af520415830278f761a7055c370f2db3b5a4
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-11-09 15:56:41 +0100

    Support task2

--- 2023/s01/src/s01.rs
+++ 2023/s01/src/s01.rs
@@ -6,4 +6,9 @@ fn solution1(input: String) -> String {
     format!("solution for {input}")
 }
 
+#[task2]
+fn solution2(input: String) -> String {
+    format!("solution for {input}")
+}
+
 make_main!();
