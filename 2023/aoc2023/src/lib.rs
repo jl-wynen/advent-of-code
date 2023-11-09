900191a217dda652b084640e477aacdbf20fcbb2
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-11-09 15:05:46 +0100

    Restructure for proc macro crate

---
+++ 2023/aoc2023/src/lib.rs
@@ -0,0 +1,65 @@
+pub mod fileio;
+
+//
+// #[macro_export]
+// macro_rules! make_part1 {
+//     ($func: ident) => {
+//         fn aoc2023_part1_wrapper(input: &str) {
+//             let output = $func(&input);
+//             println!("Part1: >|{}|<", output);
+//         }
+//     };
+// }
+//
+// #[macro_export]
+// macro_rules! make_part2 {
+//     ($func: ident) => {
+//         fn aoc2023_part2_wrapper(input: &str) {
+//             let output = $func(&input);
+//             println!("Part2: >|{}|<", output);
+//         }
+//     };
+//     () => {
+//         fn aoc2023_part2_wrapper(input: &str) {}
+//     };
+// }
+//
+// #[macro_export]
+// macro_rules! make_main {
+//     ($part1: ident) => {
+//         use aoc2023;
+//         aoc2023::make_part1!($part1);
+//
+//         fn main() {
+//             use aoc2023::fileio::read_input;
+//
+//             let input = read_input(file!());
+//             aoc2023_part1_wrapper(&input);
+//         }
+//     };
+//     ($part1: ident, $part2: ident) => {
+//         use aoc2023;
+//         aoc2023::make_part1!($part1);
+//         aoc2023::make_part2!($part2);
+//
+//         fn main() {
+//             use aoc2023::fileio::read_input;
+//
+//             let input = read_input(file!());
+//             aoc2023_part1_wrapper(&input);
+//             aoc2023_part2_wrapper(&input);
+//         }
+//     };
+// }
+//
+// #[macro_export]
+// macro_rules! init {
+//     ($part1: ident) => {
+//         use aoc2023;
+//         aoc2023::make_main!($part1);
+//     };
+//     ($part1: ident, $part2: ident) => {
+//         use aoc2023;
+//         aoc2023::make_main!($part1, $part2);
+//     };
+// }
---
+++ 2023/s01/src/s01.rs
@@ -0,0 +1,7 @@
+use aoc2023::fileio;
+use aoc2023_macro::show_streams;
+
+#[show_streams]
+fn invoke1() {}
+
+fn main() {}
