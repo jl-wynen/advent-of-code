f9f6a1d62bab5a96f64604c47faf12374768952a
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-11-29 10:32:54 +0100

    Add option for input parser

--- 2023/lib/macros.rs
+++ 2023/lib/macros.rs
@@ -1,56 +1,66 @@
 #[macro_export]
 macro_rules! make_test_cases {
-    (@step $idx:expr,) => {};
-    (@step $idx:expr, $func_name:ident, $($tail:ident,)*) => {
+    (step: $idx:expr,, parser: $parser:expr) => {};
+    (step: $idx:expr, $func_name:ident, $($tail:ident,)*, parser: $parser:expr) => {
         paste! {
                 #[test]
                 fn [<test_ $func_name>]() {
+                    use aoc2023::fileio;
                     let input = fileio::read_test_input(file!());
+                    let parsed = $parser(input);
                     let expected_output = fileio::read_test_output(file!(), $idx);
-                    let actual_output = $func_name(input).trim().to_string();
+                    let actual_output = $func_name(parsed).trim().to_string();
                     assert_eq!(actual_output, expected_output);
                 }
         }
-        make_test_cases!(@step ($idx + 1), $($tail,)*);
+        make_test_cases!(step: ($idx + 1), $($tail,)*, parser: $parser);
     };
 }
 
 #[macro_export]
 macro_rules! make_tests {
-    ($($task:ident),+) => {
+    ($parser:expr, $($task:ident),+) => {
         #[cfg(test)]
         mod __aoc2023_auto_tests {
             use super::*;
             use paste::paste;
             use aoc2023::make_test_cases;
 
-            make_test_cases!(@step 1, $($task,)*);
+            make_test_cases!(step: 1, $($task,)*, parser: $parser);
         }
     };
 }
 
 #[macro_export]
 macro_rules! make_main {
-    ($task1:ident) => {
+    ($task1:ident, parser: $parser:expr) => {
         fn main() {
             use aoc2023::fileio;
             let input = fileio::read_input(file!());
-            let res = $task1(input);
+            let parsed = $parser(input);
+            let res = $task1(parsed);
             println!("!! Task 1 result: <|{res}|>");
         }
 
-        aoc2023::make_tests!($task1);
+        aoc2023::make_tests!($parser, $task1);
     };
-    ($task1:ident, $task2:ident) => {
+    ($task1:ident, $task2:ident, parser: $parser:expr) => {
         fn main() {
             use aoc2023::fileio;
             let input = fileio::read_input(file!());
-            let res1 = $task1(input.clone());
+            let parsed = $parser(input);
+            let res1 = $task1(parsed.clone());
             println!("!! Task 1 result: <|{res1}|>");
-            let res2 = $task2(input);
+            let res2 = $task2(parsed);
             println!("!! Task 2 result: <|{res2}|>");
         }
 
-        aoc2023::make_tests!($task1, $task2);
+        aoc2023::make_tests!($parser, $task1, $task2);
+    };
+    ($task1:ident) => {
+        aoc2023::make_main!($task1, parser: std::convert::identity);
+    };
+    ($task1:ident, $task2:ident) => {
+        aoc2023::make_main!($task1, $task2, parser: std::convert::identity);
     };
 }
--- 2023/s01/src/s01.rs
+++ 2023/s01/src/s01.rs
@@ -1,11 +1,15 @@
-use aoc2023::fileio;
-
-fn task1(input: String) -> String {
+fn task1(input: usize) -> String {
+    println!("task1: {input}");
     "EXPECTED OUTPUT 1".into()
 }
 
-fn task2(input: String) -> String {
+fn task2(input: usize) -> String {
+    println!("task1: {input}");
     "EXPECTED OUTPUT 2".into()
 }
 
-aoc2023::make_main!(task1, task2);
+fn parse(input: String) -> usize {
+    input.len()
+}
+
+aoc2023::make_main!(task1, task2, parser: parse);
