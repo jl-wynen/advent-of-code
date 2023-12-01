169ac15b20dcb6e1d5b01289fa3a3820bc0e3503
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-12-01 09:02:57 +0100

    S01.1

--- 2023/s01/src/s01.rs
+++ 2023/s01/src/s01.rs
@@ -1,11 +1,29 @@
-use aoc2023::parse::decimals;
-
-fn task1(input: Vec<i64>) -> String {
-    input.iter().sum::<i64>().to_string()
+fn find_digits(s: &String) -> (u32, u32) {
+    let mut it = s.chars().filter_map(|c| c.to_digit(10));
+    let first = it.next().unwrap();
+    let second = it.fold(first, |_, c| c);
+    (first, second)
 }
 
-fn task2(input: Vec<i64>) -> String {
-    input.iter().product::<i64>().to_string()
+fn glue_digits((first, second): (u32, u32)) -> u32 {
+    first * 10 + second
 }
 
-aoc2023::make_main!(task1, task2, nom_parser: decimals);
+fn task1(input: Vec<String>) -> String {
+    input
+        .iter()
+        .map(find_digits)
+        .map(glue_digits)
+        .sum::<u32>()
+        .to_string()
+}
+
+// fn task2(input: Vec<i64>) -> String {
+//     input.iter().product::<i64>().to_string()
+// }
+
+fn parser(input: &str) -> Vec<String> {
+    input.lines().map(|x| x.to_string()).collect()
+}
+
+aoc2023::make_main!(task1, parser: parser);
