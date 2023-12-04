3aaf43c841624dc7520b5db555161e37d91f35c0
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-12-04 08:44:04 +0100

    S04.2

--- 2023/s04/src/s04.rs
+++ 2023/s04/src/s04.rs
@@ -5,24 +5,25 @@ use nom::{
     sequence::{delimited, separated_pair},
     IResult,
 };
+use std::cmp;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Card {
-    id: i64,
     winning: Vec<i64>,
     have: Vec<i64>,
+    count: i64,
 }
 
 fn parse_card(input: &str) -> IResult<&str, Card> {
-    let (input, id) = ws(decimal)(input)?;
+    let (input, _) = ws(decimal)(input)?;
     let (input, _) = ws(tag(":"))(input)?;
     let (remainder, numbers) = separated_pair(decimals, ws(tag("|")), decimals)(input)?;
     Ok((
         remainder,
         Card {
-            id,
             winning: numbers.0,
             have: numbers.1,
+            count: 1,
         },
     ))
 }
@@ -38,16 +39,24 @@ fn count_winning(card: &Card) -> i64 {
         .count() as i64
 }
 
-fn task1(input: Vec<Card>) -> i64 {
-    input
+fn task1(cards: Vec<Card>) -> i64 {
+    cards
         .iter()
         .map(count_winning)
         .map(|n| if n == 0 { 0 } else { 2i64.pow((n - 1) as u32) })
         .sum::<i64>()
 }
 
-// fn task2(input: &str) -> String {
-//     input.to_string()
-// }
+fn task2(mut cards: Vec<Card>) -> i64 {
+    let mut n_cards = 0i64;
+    for i in 0..cards.len() {
+        n_cards += cards[i].count;
+        let n_winning = count_winning(&cards[i]);
+        for j in i + 1..(cmp::min(cards.len(), n_winning as usize + i + 1)) {
+            cards[j].count += cards[i].count;
+        }
+    }
+    n_cards
+}
 
-aoc2023::make_main!(task1, nom_parser:parse);
+aoc2023::make_main!(task1, task2, nom_parser:parse);
