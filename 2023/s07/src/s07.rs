ccfd619d9382caff58d6d2d008a9157ee07ef4b7
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-12-07 10:23:41 +0100

    S07.1

--- 2023/s07/src/s07.rs
+++ 2023/s07/src/s07.rs
@@ -5,6 +5,7 @@ use nom::{
     sequence::{separated_pair, terminated},
     IResult,
 };
+use std::cmp::Ordering;
 use std::collections::HashMap;
 
 const TYPE_HIGH_CARD: u8 = 0;
@@ -15,26 +16,77 @@ const TYPE_FULL_HOUSE: u8 = 4;
 const TYPE_FOUR: u8 = 5;
 const TYPE_FIVE: u8 = 6;
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct Hand {
     cards: [u8; 5],
     ty: u8,
     bid: i64,
 }
 
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.ty.cmp(&other.ty).then_with(|| {
+            self.cards
+                .iter()
+                .zip(other.cards.iter())
+                .map(|(a, b)| a.cmp(b))
+                .filter(|&o| o != Ordering::Equal)
+                .next()
+                .unwrap_or(Ordering::Equal)
+        })
+    }
+}
+
+fn card_value(card: char) -> u8 {
+    match card {
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        'T' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => unreachable!(),
+    }
+}
+
 fn get_type(cards: &[u8; 5]) -> u8 {
-    let mut  card_counts: HashMap<u8, usize> = HashMap::new();
+    let mut card_counts: HashMap<u8, usize> = HashMap::new();
     for card in cards {
         card_counts.insert(*card, card_counts.get(card).unwrap_or(&0) + 1);
     }
-    if card_counts.len() == 1 {
-        TYPE_FIVE;
-    } else if card_counts.len() == 2 {
-        let n_first = card_counts.iter().next().unwrap().1;
+    let l = card_counts.len();
+    if l == 1 {
+        TYPE_FIVE
+    } else if l == 2 {
+        let n_first = *card_counts.iter().next().unwrap().1;
         if n_first == 2 || n_first == 3 {
             TYPE_FULL_HOUSE
-        } else { TYPE_FOUR }
-    } else if {
-        // TODO not quite right with pairs
+        } else {
+            TYPE_FOUR
+        }
+    } else if l == 3 {
+        if card_counts.iter().any(|(_, n)| *n == 3) {
+            TYPE_THREE
+        } else {
+            TYPE_TWO_PAIR
+        }
+    } else if l == 4 {
+        TYPE_ONE_PAIR
+    } else {
+        TYPE_HIGH_CARD
     }
 }
 
@@ -43,8 +95,8 @@ fn parse_hand(input: &str) -> IResult<&str, Hand> {
         separated_pair(many1(one_of("23456789TJQKA")), space1, decimal)(input)?;
     let cards: [u8; 5] = cards
         .into_iter()
-        .map(|c| c as u8)
-        .collect::<Vec<u8>>()
+        .map(card_value)
+        .collect::<Vec<_>>()
         .try_into()
         .unwrap();
     Ok((
@@ -61,8 +113,12 @@ fn parse(input: &str) -> IResult<&str, Vec<Hand>> {
     terminated(separated_list1(newline, parse_hand), newline)(input)
 }
 
-fn task1(input: Vec<Hand>) -> i64 {
-    0
+fn task1(mut input: Vec<Hand>) -> i64 {
+    input.sort();
+    input
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, hand)| acc + (i as i64 + 1) * hand.bid)
 }
 
 // fn task2(input: &str) -> i64 {
@@ -70,3 +126,19 @@ fn task1(input: Vec<Hand>) -> i64 {
 // }
 
 aoc2023::make_main!(task1, nom_parser:parse);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_type() {
+        assert_eq!(get_type(&[1, 2, 3, 4, 5]), TYPE_HIGH_CARD);
+        assert_eq!(get_type(&[1, 2, 3, 4, 3]), TYPE_ONE_PAIR);
+        assert_eq!(get_type(&[2, 2, 3, 4, 3]), TYPE_TWO_PAIR);
+        assert_eq!(get_type(&[2, 3, 3, 4, 3]), TYPE_THREE);
+        assert_eq!(get_type(&[1, 3, 3, 1, 3]), TYPE_FULL_HOUSE);
+        assert_eq!(get_type(&[1, 3, 1, 1, 1]), TYPE_FOUR);
+        assert_eq!(get_type(&[1, 1, 1, 1, 1]), TYPE_FIVE);
+    }
+}
